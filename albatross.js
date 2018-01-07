517d2cc6e2db7b7431d7dc7423140a2cfe7acefb
Alex Sperellis <asperellis@gmail.com>
2018-01-07 11:04:02 -0500

    getTrade func added

--- albatross.js
+++ albatross.js
@@ -20,7 +20,20 @@ const init = () => {
   axios.all([ Exchanges.gdax.getTicker(), Exchanges.poloniex.getTicker(), Exchanges.binance.getTicker()])
   .then(axios.spread((gdax, poloniex, binance) => {
     // compare all possible market pairs for each coin - makes sure they arent undefined
-    Calc.compare(Exchanges.groupByCoin([...gdax, ...poloniex, ...binance].filter((m) => m)), TRIGGER, Wallets);
+    const trade = Calc.getTrade(Exchanges.groupByCoin([...gdax, ...poloniex, ...binance].filter((m) => m)), TRIGGER);
+    
+    if(trade) {
+      // Stop the loop
+      clearInterval(loop);
+      // TODO: AL, what do we need to know here to make a trade?
+      console.log('TRADING: ', trade);
+      // TODO: Exec a trade, ONLY if wallet amounts will allow
+      // TODO: Start the loop again after the trade is complete.
+      // loop = setInterval(init, TIME);
+    } else {
+      // Continue on - placeholder logging for now - ideally this wouldn't even be there.
+      console.log('NO TRADE AVAILABLE ON THIS LOOP ITERATION');
+    }
   })).catch(error => {
     console.log(error);
   });
--- utils/calculations.js
+++ utils/calculations.js
@@ -1,47 +1,61 @@
 const FEES = require('./../exchanges/exchanges').fees;
 
 const calculations = {
-  getMargin: function(market1, market2, coin) {
+  getMargin: function(market1, market2) {
     // theoretical 1 BTC currency input but it doesnt matter.
     const curr = 1;
     return ((curr / market2.ask * (1 - market2.fees.taker) * market1.bid * (1 - market1.fees.taker)) - curr) / curr;
   },
-  // compares all coins market prices. if margin meets trigger. fire trade
-  compare: function(coins, trigger, wallets) {
+  // compares all coins market prices. if margin meets trigger. return trade
+  getTrade: function(coins, trigger) {
+    // trade to return
+    let trade = {};
+
+    // compares current highest trade and a new one
+    const compareTrades = (current, newest) => {
+      // keep current if theres a trade and its higher than the new
+      if ((current.net && current.net > newest.net)) {
+        return current;
+      }
+
+      return newest;
+    };
+    
+    // for each coin and its available market pairs
     for(coin in coins) {
       // all the markets for that coin
       let markets = coins[coin];
       // only check if there's at least two markets to compare
       while(markets.length > 1) {
+        // current market
         const m = markets.pop();
+        // compare with all remaining markets
         for(let k = 0; k < markets.length; k++) {
           const market1 = m;
           const market2 = markets[k];
           // add in the fees
           market1.fees = FEES[market1.market];
           market2.fees = FEES[market2.market];
-          // TODO: dont even run these funcs if you dont have the coin in the wallets
-          console.log(this.getMargin(market1, market2, coin), coin, 'margin for', market1.market, 'to', market2.market);
-          console.log(this.getMargin(market2, market1, coin), coin, 'margin for', market2.market, 'to', market1.market);
-          // TODO: Run Calculations here for each market pair
-          // TODO: If Calculations meet trigger - fire the trade
-        }
-      }
-    }
-  },
-  canTrade: function(pair, wallet) {
-    let canTrade = true;
-    
-    for (entry in wallet) {
-      if (pair.indexOf(entry)) {
-        if (wallet[entry] === 0) {
-          canTrade = false;
-          break;
+          // calc nets
+          const m1ToM2 = this.getMargin(market1, market2);
+          const m2ToM1 = this.getMargin(market2, market1);
+          // see if either pair beats the current trades net
+          if(m1ToM2 > m2ToM1) {
+            trade = compareTrades(trade, { exchanges: market1.market + '-' + market2.market, net: m1ToM2 });
+          } else {
+            trade = compareTrades(trade, { exchanges: market1.market + '-' + market2.market, net: m2ToM1 });
+          }
         }
       }
     }
 
-    return canTrade;
+    // if there's a trade and the net is not greater than our min allowed for a trade then return nothing
+    if (trade.net && trade.net < trigger) {
+      return false;
+    }
+
+    // return the trade and all info needed to exec on any exchange
+    return trade;
   }
 };
 
