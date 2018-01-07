8448773c1db488f48c941f44f8b09b670b0c53b5
Alex Sperellis <asperellis@gmail.com>
2018-01-07 11:18:03 -0500

    no more timed intervals, use trade as condition

--- albatross.js
+++ albatross.js
@@ -10,10 +10,6 @@ const Calc = require('./utils/calculations');
 
 // constants
 const TRIGGER = 0.001;
-const TIME = 2000;
-
-// for timer
-let loop;
 
 const init = () => {
   // get all exchange ticker data
@@ -23,20 +19,18 @@ const init = () => {
     const trade = Calc.getTrade(Exchanges.groupByCoin([...gdax, ...poloniex, ...binance].filter((m) => m)), TRIGGER);
     
     if(trade) {
-      // Stop the loop
-      clearInterval(loop);
       // TODO: AL, what do we need to know here to make a trade?
       console.log('TRADING: ', trade);
       // TODO: Exec a trade, ONLY if wallet amounts will allow
-      // TODO: Start the loop again after the trade is complete.
-      // loop = setInterval(init, TIME);
+      // TODO: Start the search again after the trade is complete.
+      // init();
     } else {
-      // Continue on - placeholder logging for now - ideally this wouldn't even be there.
-      console.log('NO TRADE AVAILABLE ON THIS LOOP ITERATION');
+      init();
     }
   })).catch(error => {
     console.log(error);
   });
 };
 
-loop = setInterval(init, TIME);
+init();
+// loop = setInterval(init, TIME);
